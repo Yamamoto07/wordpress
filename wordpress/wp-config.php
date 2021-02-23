62e6c87ecfa5d988ad1b4e67ed9a96f3045862df
Yamamoto Ryosuke <rxatv31191@yahoo.co.jp>
2021-02-23 17:59:52 +0900

    wp-config.phpにunix_socketを追加

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -45,11 +45,11 @@ define( 'DB_COLLATE', '' );
 
 //例外処理
 try{
-    $db=new PDO('mysql:dbname=heroku_be9ffcecb28fb7b;host=us-cdbr-east-03.cleardb.com;charset=utf8','b6037e96bc0d6f','63f89a45');
+    $db=new PDO('mysql:dbname=heroku_be9ffcecb28fb7b;host=us-cdbr-east-03.cleardb.com;unix_socket=/tmp/mysql.sock;charset=utf8','b6037e96bc0d6f','63f89a45');
 }catch(PDOException $e){
     print('DB接続エラー:'.$e->getMessage());
 }
-
+/tmp/mysql.sock
 /**#@+
  * 認証用ユニークキー
  *
